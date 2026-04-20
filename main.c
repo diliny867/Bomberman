8cba3e16081411f2eb1952c1b0148570864b6c52
ak24233 <ak24233@edu.lu.lv>
2026-04-20 22:36:28 +0300

    Update main.c
    ?

--- main.c
+++ main.c
@@ -15,5 +15,20 @@ int main(int argc, char **argv) {
         }
     }
 
+    /*if(argc > 1 && argv[1][0] == '-' && argv[1][1] == 's') {
+        server();
+    } else {
+        // default values
+        const char *ip = "127.0.0.1";
+        int port = 8080;
+
+        if(argc >= 3) {
+            ip = argv[1];
+            port = atoi(argv[2]);
+        }
+
+        client(ip, port);
+    }*/
+
     return 0;
 }
